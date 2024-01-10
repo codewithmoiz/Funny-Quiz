20e5a8a6f293a3f116b71e1ea2cb7b3327932327
Abdul Moiz <138908943+codewithmoiz@users.noreply.github.com>
2024-01-10 23:58:33 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-
+var face_1 = document.querySelector(".face1");
+var face_2 = document.querySelector(".face2");
+var face_3 = document.querySelector(".face3");
+var face_4 = document.querySelector(".face4");
 function face1() {
-    gsap.to(".face1", {
+    gsap.to(face_1, {
         duration: 3,
         x: 680,
         y: 50,
@@ -16,7 +19,7 @@ function face1() {
 }
 
 function face2() {
-    gsap.to(".face2", {
+    gsap.to(face_2, {
         duration: 3,
         x: 675,
         y: -60,
@@ -32,7 +35,7 @@ function face2() {
 }
 
 function face3() {
-    gsap.to(".face3", {
+    gsap.to(face_3, {
         duration: 3,
         x: 677,
         y: -160,
@@ -48,7 +51,7 @@ function face3() {
 }
 
 function face4() {
-    gsap.to(".face4", {
+    gsap.to(face_4, {
         duration: 3,
         x: 675,
         y: -260,
@@ -63,8 +66,8 @@ function face4() {
     });
 }
 
-function resetFaces() {
-    gsap.to(".face1, .face2, .face3, .face4", {
+function resetTheFacesOfDevil() {
+    gsap.to(face_1,face_2,face_3,face_4, {
         duration: 2,
         x: 0,
         y: 0
